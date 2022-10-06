73c5c9c0db9d95db11e7d6335965b834167451cc
JosueLeit <josueleite@outlook.com>
2022-10-05 22:04:56 -0300

    component sidebar

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Sidebar.module.css";
+
+export default function () {
+  return (
+    <aside className={styles.sidebar}>
+      clientes <br /> faturamento
+    </aside>
+  );
+}
