20ca31ea00856eaa7765987d0233a0fc6c8611db
JosueLeit <josueleite@outlook.com>
2022-10-06 22:35:20 -0300

    instaling react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import {
+  createBrowserRouter,
+  Switch,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import "./global.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello BrowserRouter</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Route exact path="/" component={App} />
+    <Route exact path="/about" component={About} />
+    {/* <App /> */}
   </React.StrictMode>
 );
+
+// import About from "./components/views/About";
+// import Home from "./components/views/Home";
+// import Layout from "./components/views/Layout";
+
+const Routes = () => (
+  <BrowserRouter>
+    <Layout>
+      <Switch></Switch>
+    </Layout>
+  </BrowserRouter>
+);
+
+export default Routes;
