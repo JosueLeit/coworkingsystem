30aa6933d933b1df811a496449fe5d8bc1d1bf4d
JosueLeit <josueleite@outlook.com>
2022-10-18 22:42:31 -0300

    fixing css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,19 @@ export function Header() {
       <div>
         <strong>
           <h2>Registro de Usuários</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+            adipisci architecto hic iure odio sed reiciendis ab illum
+            reprehenderit illo tempora magni id! Dicta illo ad velit corrupti
+            repudiandae rem.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rem
+            repellat nisi, voluptatem cumque, nemo, voluptatibus sequi error
+            voluptates molestiae tempora consequuntur. Perspiciatis fuga odit
+            officiis porro tenetur quasi eos.
+          </p>
         </strong>
       </div>
     </header>
