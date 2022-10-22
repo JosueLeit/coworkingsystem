d4cf8b2d6702b6aa6eed3b518a9842bab1766cd8
JosueLeit <josueleite@outlook.com>
2022-10-21 21:42:39 -0300

    fixing Styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header";
 // import "./global.css";
 // import styles from "./App.module.css";
 
-export function App() {
+export default function App() {
   return (
     <div>
       <Header />
--- src/contacts.jsx
+++ src/contacts.jsx
@@ -1,4 +1,4 @@
-import localforage from "localforage";
+// import localforage from "localforage";
 import { matchSorter } from "match-sorter";
 import sortBy from "sort-by";
 
